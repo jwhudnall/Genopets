921e15ed2bd608a55bf1ee79e1f2c580f4f69bc6
James Hudnall <jimmyhudnall@gmail.com>
2022-05-13 21:27:32 -0400

    Fix conversion calculation

--- energyConversion.js
+++ energyConversion.js
@@ -1,11 +1,11 @@
 const getInterval = (steps) => {
-  return steps / 1000;
+  return Math.floor(steps / 1000);
 };
 
 const getEnergy = (totalSteps, decay) => {
   let energy = 0;
   for (let i = 1; i <= totalSteps; i++) {
-    if (i <= 1000) {
+    if (i <= 1) {
       energy += 1;
     } else {
       energy += (1 - decay) ** getInterval(i);
@@ -14,12 +14,12 @@ const getEnergy = (totalSteps, decay) => {
   return energy;
 };
 
-const result3k = getEnergy(3000, 0.046);
-const result5k = getEnergy(5000, 0.046);
-const result8k = getEnergy(8000, 0.046);
-const result10k = getEnergy(10000, 0.046);
-const result15k = getEnergy(15000, 0.046);
-const result20k = getEnergy(20000, 0.046);
+const result3k = getEnergy(3000, 0.055);
+const result5k = getEnergy(5000, 0.055);
+const result8k = getEnergy(8000, 0.055);
+const result10k = getEnergy(10000, 0.055);
+const result15k = getEnergy(15000, 0.055);
+const result20k = getEnergy(20000, 0.055);
 
 console.log(`Final Energy for 3,000 steps: ${result3k}`);
 console.log(`Final Energy for 5,000 steps: ${result5k}`);
