847d72dd629d4cf4c687ba517e847d482b60aa84
James Hudnall <jimmyhudnall@gmail.com>
2022-05-16 21:28:03 -0400

    Save progress for energy conversion

--- energyConversion.js
+++ energyConversion.js
@@ -15,20 +15,53 @@ const getEnergy = (totalSteps, decay) => {
   return energy;
 };
 
-const getEnergyNew = (bankedSteps, stepEfficiencyRating) => {
-  // convert and return steps
+// Currently DOES NOT WORK. Progress saved.
+const getEnergyNew = (bankedSteps, petLevel) => {
+  const getInterval = (steps) => {
+    return Math.floor(steps / 1000);
+  };
+
+  const getSER = (petLevel) => {
+    if (petLevel > 77) {
+      return 4.8;
+    } else if (petLevel < 1) {
+      return null;
+    }
+    return 1.0 + 0.05 * (petLevel - 1);
+  };
+
+  const Decay = 6;
+  const SER = getSER(petLevel);
+
+  const SEF = (Decay - (SER - 1)) / 100;
+  const Ei = 1;
+  const nurture = 1;
+
+  let energy = 0;
+
+  for (let i = 1; i <= bankedSteps; i++) {
+    if (i == 1) {
+      energy += 1;
+    } else {
+      energy += bankedSteps * (Ei * (1 - SEF) ** getInterval(bankedSteps)) * nurture;
+    }
+  }
+  return energy;
 };
 
-const result3k = getEnergy(3000, 0.055);
-const result5k = getEnergy(5000, 0.055);
-const result8k = getEnergy(8000, 0.055);
-const result10k = getEnergy(10000, 0.055);
-const result15k = getEnergy(15000, 0.055);
-const result20k = getEnergy(20000, 0.055);
+const result10k = getEnergyNew(10000, 1);
+console.log(`Final Energy for 10k steps: ${result10k}`);
 
-console.log(`Final Energy for 3,000 steps: ${result3k}`);
-console.log(`Final Energy for 5,000 steps: ${result5k}`);
-console.log(`Final Energy for 8,000 steps: ${result8k}`);
-console.log(`Final Energy for 10,000 steps: ${result10k}`);
-console.log(`Final Energy for 15,000 steps: ${result15k}`);
-console.log(`Final Energy for 20,000 steps: ${result20k}`);
+// const result3k = getEnergy(3000, 0.055);
+// const result5k = getEnergy(5000, 0.055);
+// const result8k = getEnergy(8000, 0.055);
+// const result10k = getEnergy(10000, 0.055);
+// const result15k = getEnergy(15000, 0.055);
+// const result20k = getEnergy(20000, 0.055);
+
+// console.log(`Final Energy for 3,000 steps: ${result3k}`);
+// console.log(`Final Energy for 5,000 steps: ${result5k}`);
+// console.log(`Final Energy for 8,000 steps: ${result8k}`);
+// console.log(`Final Energy for 10,000 steps: ${result10k}`);
+// console.log(`Final Energy for 15,000 steps: ${result15k}`);
+// console.log(`Final Energy for 20,000 steps: ${result20k}`);
